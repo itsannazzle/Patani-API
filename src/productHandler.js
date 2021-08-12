35ff920d30e0b15d6f9be7cfd546f160f701f1b2
itsannazzle <akarenina25@gmail.com>
2021-08-13 00:14:28 +0700

    adding parameter endpoint

--- src/productHandler.js
+++ src/productHandler.js
@@ -9,9 +9,13 @@ const addNewProduct = async (request, h) => {
         price,
         amount,
         desc,
+        imgUrl,
+        locSeller,
+        waktuPanen,
+        kategori,
     } = request.payload;
     console.log(`title : ${title.length}`);
-        console.log(`price : ${price.length}`);
+    console.log(`price : ${price.length}`);
 
     const productValidation = title.length > 3 && price.length >= 3;
 
@@ -21,13 +25,18 @@ const addNewProduct = async (request, h) => {
         price,
         amount,
         desc,
+        imgUrl,
+        locSeller,
+        waktuPanen,
+        kategori,
     };
     console.log(productValidation);
     if (productValidation) {
         products.push(newProduct);
-        await client.query(`insert into products(id_user, title, price, amount, description) 
-       values($1, $2, $3, $4, $5) RETURNING *`, [`${idUser}`, `${title}`,
-       `${price}`, `${amount}`, `${desc}`]);
+        await client.query(`insert into products(id_user, title, price, amount, 
+            description, imageurl, locationseller, waktupanen, kategori) 
+       values($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, [`${idUser}`, `${title}`,
+       `${price}`, `${amount}`, `${desc}`, `${imgUrl}`, `${locSeller}`, `${waktuPanen}`, `${kategori}`]);
        return response.code(200);
     }
     return response.code(400);
@@ -38,9 +47,7 @@ const getAllProduct = async (request, h) => {
     try {
         const result = await client.query(`select * from products where id_user = $1`, [`${idUser}`]);
         console.log(result.rows);
-        return h.response({
-            products: result.rows,
-        }).code(200);
+        return h.response(result.rows).code(200);
     } catch (error) {
         console.log(error);
         h.response().code(404);
